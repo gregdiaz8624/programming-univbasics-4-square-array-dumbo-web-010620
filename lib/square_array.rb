3b1450a0b21aad300944a72bf3cf9fac93f1524b
Gregory Diaz <gregdiaz8624@yahoo.com>
2019-12-29 21:39:42 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,3 @@
-# def square_array(array)
-#   squared = []
-#   count = 0
-#   while count < array.size do
-#     squared << (array[count] ** 2)
-#     counter = count + 1
-#   end
-#   squared
-# end 
-
 def square_array(array)
   squared = []
   count = 0
